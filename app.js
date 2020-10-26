0565681ac2666514d8da5a465e5662050895186d
mapinto25 <mapinto@ucsd.edu>
2020-10-26 16:56:58 -0700

    added first api endpoint

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "barber_shop",
+  database: "sys"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+});
+
+app.get('/shop', function (req, res) {
+  con.query('SELECT * FROM shops', function (err, result, fields) {
+     if (err) throw err;
+     return res.json(result)
+   });
+})   
+
 
 app.get('/', (req, res) => {
   res.send('We are on!')
